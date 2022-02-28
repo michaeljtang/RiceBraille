ee8c6ce276dd87878819c1e79b20204003fb0f03
JustinCheung168 <jrc17@rice.edu>
2022-02-28 13:49:39 -0600

    Adding finger-tracker.py

---
+++ finger-tracker.py
@@ -0,0 +1,91 @@
+#finge tracking
+import cv2
+import mediapipe as mp
+import pickle
+import numpy as np
+import pandas as pd
+import csv
+mp_drawing = mp.solutions.drawing_utils # Drawing helpers
+mp_holistic = mp.solutions.holistic # Mediapipe Solutions
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture('5.mp4')
+# Initiate holistic model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        # Make Detections
+        results = holistic.process(image)
+        # print(results.face_landmarks)
+
+        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+
+        # Recolor image back to BGR for rendering
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # 1. Draw face landmarks
+        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                                  mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                                  )
+
+        # 2. Right hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                                  )
+
+        # 3. Left Hand
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                                  )
+
+        # 4. Pose Detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  )
+        # Export coordinates
+
+            # Extract Pose landmarks
+        with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+            res = hands.process(image)
+            pose_row = []
+            pose = res.multi_hand_landmarks
+            print(pose)
+            try:
+                pose_row = list(
+                    np.array([[landmark.x, landmark.y, landmark.z] for landmark in pose]).flatten())
+
+            # Concate rows
+                row = pose_row
+                class_name = 'hand_landmarks'
+            # Append class name
+                row.insert(0, class_name)
+            except:
+                pass
+
+
+            # Export to CSV
+        with open('finger.csv', mode='a', newline='') as f:
+                csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(row)
+
+
+
+
+
+        cv2.imshow('Raw Webcam Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
